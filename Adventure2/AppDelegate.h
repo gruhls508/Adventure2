aeffef7681ea866ce65ea71e25ba4a6e94d4472f
Glen Ruhl <glenruhl@glens-air.hsd1.il.comcast.net>
2014-07-23 04:24:27 -0500

    Initial Commit

---
+++ Adventure2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure2
+//
+//  Created by Glen Ruhl on 7/23/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Adventure2Tests/Adventure2Tests.m
@@ -0,0 +1,34 @@
+//
+//  Adventure2Tests.m
+//  Adventure2Tests
+//
+//  Created by Glen Ruhl on 7/23/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Adventure2Tests : XCTestCase
+
+@end
+
+@implementation Adventure2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
