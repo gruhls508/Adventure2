10b88de2ec020a6fe194786473195528fc2bfb80
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-10-04 17:42:53 -0400

    properly showing copyright

--- SyrupViewController.m
+++ SyrupViewController.m
@@ -3,7 +3,7 @@
 //  Adventure2
 //
 //  Created by Glen Ruhl on 7/23/14.
-//  Copyright (c) 2014 MM. All rights reserved.
+//  Copyright (c) 2014 Intradine. All rights reserved.
 //
 
 #import "SyrupViewController.h"
