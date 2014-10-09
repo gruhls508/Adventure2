42b5dc0e622b6483d4bf57c04f850965b2de3be0
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-10-09 18:18:46 -0400

    more code cleanup--StoryVC

--- TrainChoiceViewController.m
+++ TrainChoiceViewController.m
@@ -14,14 +14,10 @@
 
 @implementation TrainChoiceViewController
 
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
-
 
 
 @end
--- storyViewController.m
+++ storyViewController.m
@@ -9,21 +9,11 @@
 #import "storyViewController.h"
 
 @interface storyViewController ()
-@property (weak, nonatomic) IBOutlet UIButton *clearUpBoxesButton;
-@property (weak, nonatomic) IBOutlet UIButton *eatBoxesButton;
+
 
 @end
 
 @implementation storyViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
@@ -31,17 +21,6 @@
   
 }
 
-
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    UIViewController *vc = segue.destinationViewController;
-    
-    if ([segue.identifier isEqualToString:@"clearUpBoxesSegue"]){
-        vc.navigationItem.title = self.clearUpBoxesButton.currentTitle;
-    }
-    else if ([segue.identifier isEqualToString:@"clearUpBoxesSegue"]){
-        vc.navigationItem.title = self.eatBoxesButton.currentTitle;
-    }
-}
 
 
 
